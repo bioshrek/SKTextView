e20a2474ac1b5d5bd92f1f77e40f04464eae0021
bioshrek <shrekwang1@gmail.com>
2014-10-31 18:05:06 +0800

    initial

---
+++ SKTextViewDemo/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  SKTextViewDemo
+//
+//  Created by Shrek Wang on 10/31/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SKTextViewDemo/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  SKTextViewDemo
+//
+//  Created by Shrek Wang on 10/31/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
